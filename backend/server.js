e24a99796d607b2ee99edce50a2dfcc9cd298a7a
Ishu Raj Gupta <ishuraj5963@gmail.com>
2025-06-20 23:34:30 +0530

    vercel URL

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,17 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const allowedOrigin = "https://collaborative-whiteboard-with-real-time-drawing-41bfty2dl.vercel.app";
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://collaborative-whiteboard-with-real-time-drawing-41bfty2dl.vercel.app"
+];
 app.use(cors({
-  origin: allowedOrigin,
+  origin: allowedOrigins,
   credentials: true // if you use cookies/auth, otherwise can be omitted
 }));
 const io = socketIo(server, {
   cors: {
-    origin: allowedOrigin,
+    origin: allowedOrigins,
     methods: ["GET", "POST"]
   }
 });
